8116d60f6d39f611f3b71e28b79671ce0f3b3e46
Justinas <justasrimavicius@gmail.com>
2025-01-08 11:37:34 +0200

    moved .json file to base64

--- firestore_client.py
+++ firestore_client.py
@@ -1,7 +1,13 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+import json
+import base64
 
-cred = credentials.Certificate("./firebaseServiceAccountKey.json")
+firebase_key_base64 = os.getenv('FIREBASE_KEY_BASE64')
+firebase_key = json.loads(base64.b64decode(firebase_key_base64).decode('utf-8'))
+
+cred = credentials.Certificate(firebase_key)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
