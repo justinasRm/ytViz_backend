048b8581e0767e5565182d767f6bd6dde2499b12
Justinas <justasrimavicius@gmail.com>
2025-01-08 10:43:33 +0200

    firebase service json change

--- firestore_client.py
+++ firestore_client.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("./secure/firebaseServiceAccountKey.json")
+cred = credentials.Certificate("./firebaseServiceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
