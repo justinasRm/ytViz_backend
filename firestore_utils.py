0752f576088f86c0b942c8c5436bc4bc8702bc12
Justinas <justasrimavicius@gmail.com>
2025-01-10 14:19:43 +0200

    change of api quota saving logic

--- firestore_utils.py
+++ firestore_utils.py
@@ -1,21 +1,42 @@
 from firestore_client import get_firestore_client
 from google.cloud import firestore
+from datetime import datetime
+
 db = get_firestore_client()
 
 def getAPIQuota():
     '''
     Get API quota from Firestore.
     '''
-    doc_ref = db.collection('APIQuota').document('APIQuota')
-    doc = doc_ref.get().to_dict()
-    return doc['currNum']
+    currentDay = f'{datetime.now().year}-{datetime.now().month}-{datetime.now().day}'
+
+    doc_ref = db.collection('APIQuota').document(currentDay)
+
+    if not doc_ref.get().exists:
+        doc_ref.set({
+            'quotaUsed': 0
+        })
+        return 0
+    else:
+        return doc_ref.get().to_dict()['quotaUsed']
+
 
 def updateAPIQuota(add: int):
     '''
     Update API quota in Firestore.
     '''
-    doc_ref = db.collection('APIQuota').document('APIQuota')
-    doc_ref.set({
-        'currNum': firestore.Increment(add)
-    }, merge=True)
+    currentDay = f'{datetime.now().year}-{datetime.now().month}-{datetime.now().day}'
+    
+    doc_ref = db.collection('APIQuota').document(currentDay)
+    doc_snapshot = doc_ref.get()
+
+    if doc_snapshot.exists:
+        doc_ref.update({
+            'quotaUsed': firestore.Increment(add)
+        })
+    else:
+        doc_ref.set({
+            'quotaUsed': add
+        })
+
     return {"message": "Data updated successfully."}
