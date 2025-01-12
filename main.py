38e9495661de3c070c3d17d2c61bd85726e12fd5
Justinas <justasrimavicius@gmail.com>
2025-01-12 11:24:15 +0200

    cors origin change

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from firestore_utils import getAPIQuota
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["https://yt-viz-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
